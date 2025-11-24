9374a710d5f87a08a5910fef3225f0f8f061ec19
mouadbimk <mouadbimekliouen@gmail.com>
2025-11-24 16:52:45 +0100

    add Error Handling & Submiting forms & handling forms error

--- 05-fast-react-pizza/src/App.jsx
+++ 05-fast-react-pizza/src/App.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { Loader as menuLoader } from "./features/menu/Menu";
+
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import Order, { Loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,11 +23,21 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order", element: <Order /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
